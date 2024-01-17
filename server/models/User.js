9c50080f0712b027572d84434e36a9ea8e566603
sus53 <manandharsrijan53@gmail.com>
2024-01-17 11:44:11 +0545

    Server created with auth complete

---
+++ server/models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    picturePath: {
+        type: String,
+        default: ""
+    },
+    friends: {
+        type: Array,
+        default: []
+    },
+    location: String,
+    occupation: String,
+    viewdProfile: Number,
+    impression: Number
+}, { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
